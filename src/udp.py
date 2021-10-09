5bbf5f33066a4bca8dbd4c7b9a09176559eccd22
Niklas Loeser <51879435+data-niklas@users.noreply.github.com>
2021-10-09 21:17:29 +0200

    add udp locking

--- src/udp.py
+++ src/udp.py
@@ -1,11 +1,15 @@
 import socket
+from threading import Lock
 
 
 UDP_IP = "127.0.0.1"
 UDP_RECEIVE_PORT = 15730
 UDP_SEND_PORT = 15731
 
+udp_lock = Lock()
+
 def send_recv_udp(message):
+    udp_lock.acquire()
     send_sock = socket.socket(socket.AF_INET, # Internet
                          socket.SOCK_DGRAM) # UDP
     recv_sock = socket.socket(socket.AF_INET, # Internet
@@ -15,5 +19,10 @@ def send_recv_udp(message):
     send_sock.sendto(message, (UDP_IP, UDP_SEND_PORT))
 
     data, addr = recv_sock.recvfrom(1024) # buffer size is 1024 bytes
+
+    send_sock.close()
+
+    recv_sock.close()
+    udp_lock.release()
 
     return data
