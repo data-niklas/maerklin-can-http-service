65ba835e75db049775c6ae8507123ba593cb0014
imkgerC <imrund08@gmail.com>
2022-03-08 10:44:05 +0100

    add raw database read

--- src/app/services/database_read/__init__.py
+++ src/app/services/database_read/__init__.py
@@ -11,6 +11,7 @@ from odata_query.sqlalchemy import apply_odata_query
 
 from app.models.can_message import Base
 from app.models.can_message_converter import registered_models
+from app.models.raw_can_message import CANMessage
 from config import get_settings
 
 router = APIRouter()
@@ -18,12 +19,18 @@ router = APIRouter()
 settings = get_settings()
 
 DB = settings.high_level_db_dump_database
+RAW_DB = settings.raw_db_dump_database
 
 engine = create_async_engine(
     DB, connect_args={"check_same_thread": False}
 )
 
+raw_engine = create_async_engine(
+    RAW_DB, connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession, expire_on_commit=False)
+SessionLocalRaw = sessionmaker(autocommit=False, autoflush=False, bind=raw_engine, class_=AsyncSession, expire_on_commit=False)
 
 @router.on_event("startup")
 async def on_startup() -> None:
@@ -35,9 +42,13 @@ async def get_db():
     async with SessionLocal() as session:
         yield session
 
-def make_get(model):
+async def get_raw_db():
+    async with SessionLocalRaw() as session:
+        yield session
+
+def make_get(model, db_getter=get_db):
     @router.get(f"/get{model.__name__}/", response_model=LimitOffsetPage[sqlalchemy_to_pydantic(model)])
-    async def get(filter: Optional[str] = None, db: AsyncSession = Depends(get_db)):
+    async def get(filter: Optional[str] = None, db: AsyncSession = Depends(db_getter)):
         query = select(model)
         if filter is not None and len(filter) > 0:
             query = apply_odata_query(query, filter)
@@ -45,5 +56,6 @@ def make_get(model):
 
 for model in registered_models:
     make_get(model)
+make_get(CANMessage, get_raw_db)
 
 add_pagination(router)
