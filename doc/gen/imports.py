6a73a29bfd72f9a335cab0be60449e78c3a09e02
Niklas Loeser <51879435+data-niklas@users.noreply.github.com>
2022-03-13 01:00:34 +0100

    adds wordcloud generation

--- doc/gen/imports.py
+++ doc/gen/imports.py
@@ -6,8 +6,23 @@ import utils
 from start import SCRIPTS, EXTERNAL_SERVICES
 
 source_dir = "../../src/"
+base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "out")
+general_path = os.path.join(base_dir, "imports.puml")
 # patched from src/start.py
 ASGI_SERVICES = ["raw_can_recv", "raw_can_send", "high_level_can_recv", "high_level_can_send", "high_level_can", "database_read"]
+
+STYLE = """
+<style>
+    classDiagram{
+        FontColor Black
+        BackgroundColor White
+        LineThickness 2
+        LineColor Black
+        RoundCorner 10
+    }
+</style>
+
+"""
 
 
 def path_from_import(import_):
@@ -80,7 +95,21 @@ class ImportListVisitor(ast.NodeVisitor):
             self.imports.update(subimports)
         return self.imports
 
+def print_tree(tree, f):
+    for key in tree:
+        f.write(f"namespace {key} {{\n")
+        print_tree(tree[key], f)
+        f.write(f"}}\n")
+
 if __name__ == "__main__":
+    f = open(general_path, "w")
+    f.write(f"@startuml\n")
+    f.write("skinparam useBetaStyle true\n")
+    f.write("skinparam linetype ortho\n")
+    f.write(STYLE)
+
+
+
     interesting_points = {}
     for service in EXTERNAL_SERVICES:
         external_services_path = os.path.join(source_dir, "app", "external_services")
@@ -97,9 +126,29 @@ if __name__ == "__main__":
         return any(point in path for point in interesting_points.keys())
     def what_interesting(path):
         return [point for point in interesting_points.keys() if point in path.split(".")][0]
+
+
+    tree = dict()
+
     for point, path in interesting_points.items():
         print(point + " depends on:")
+        clean_path = path[path.find("app"):].replace("/", ".")
+        last_branch = tree
+        for part in clean_path.split("."):
+            if part not in last_branch:
+                last_branch[part] = dict()
+            last_branch = last_branch[part]
+        print(tree)
         imports = visitor.get_all_imports_from(path)
         # print(set(import_ for import_ in imports if not is_interesting(import_)))
         # print(set(what_interesting(import_) for import_ in imports if is_interesting(import_)))
-        print(set(path_from_import(import_)[len(source_dir):].replace(os.sep, ".") for import_ in imports if "app" in import_.split(".")))
+        for dependency in set(path_from_import(import_)[len(source_dir):].replace(os.sep, ".") for import_ in imports if "app" in import_.split(".")):
+            
+            f.write(f'{dependency.replace(".py","")} <|-- {clean_path}\n')
+
+    print_tree(tree, f)
+        #print(set(path_from_import(import_)[len(source_dir):].replace(os.sep, ".") for import_ in imports if "app" in import_.split(".")))
+
+    f.write(f"@enduml")
+    f.close()
+    os.system(f"plantuml -tsvg {general_path}")
