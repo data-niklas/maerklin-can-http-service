fc57269c4bb8d01f468c6e8fd7840f2c33f2d2b0
imkgerC <imrund08@gmail.com>
2022-03-13 19:30:09 +0100

    add stopword removal and remove collocations

--- doc/gen/wordcloud.sh
+++ doc/gen/wordcloud.sh
@@ -1,3 +1,3 @@
 #!/bin/env sh
 find ../../src . -type f | grep -F ".py" | grep -v "__pycache" | xargs cat | sed '/^$/d' > out/code.txt
-cat out/code.txt | wordcloud_cli --mask maerklin.jpg --colormask maerklin.jpg --imagefile out/wordcloud.png
+wordcloud_cli --text out/code.txt --mask maerklin.jpg --colormask maerklin.jpg --imagefile out/wordcloud.png --stopwords stopwords.txt --no_collocations
